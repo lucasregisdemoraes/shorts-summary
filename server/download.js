0e87f0dc1f3e8e3d2295ba6640c4100ee936a1a8
lucasregisdemoraes <lucasgrm03@gmail.com>
2023-09-23 14:28:39 -0300

    finish class 3

--- server/download.js
+++ server/download.js
@@ -1,7 +1,7 @@
 import ytdl from 'ytdl-core'
 import fs from 'fs'
 
-export const download = (videoId) => {
+export const download = (videoId) => new Promise((resolve, reject) => {
     const videoURL = "https://youtube.com/shorts/" + videoId
     console.log("download:", videoId)
 
@@ -15,10 +15,11 @@ export const download = (videoId) => {
         })
         .on("end", () => {
             console.log("Download do vídeo finalizado.")
+            resolve()
         })
         .on("error", (error) => {
-            const newLocal = "Não "
-        console.log("Não foi possível fazer o download do vídeo. Detalhes do erro:", error)
+            console.log("Não foi possível fazer o download do vídeo. Detalhes do erro:", error)
+            reject()
         })
         .pipe(fs.createWriteStream("./tmp/audio.mp4"))
-}
+})
